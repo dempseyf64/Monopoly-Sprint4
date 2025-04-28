cc34c70323e60ad28071aa2c972475e588f82492
Kristian <Wrightk@moravian.edu>
2025-04-28 03:29:03 -0400

    Update package.sh

--- package.sh
+++ package.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-# Clean the project, removing any previously compiled files and build artifacts
-mvn clean
-
-# Compile the project's source code
-mvn compile
-
-# Run the unit tests to ensure code correctness and functionality
-mvn test
+# Clean the project and package it into a JAR file
+mvn clean package
